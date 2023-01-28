1ac035f45f039af085716b2dcfdab7bbd081a22f
PetranaKoleva <petrana9094@gmail.com>
2023-01-28 17:23:48 +0000

    Add new test

--- Collection.UNitTests/CollectionTests.cs
+++ Collection.UNitTests/CollectionTests.cs
@@ -1,16 +1,39 @@
+
+using Collections;
+
 namespace Collection.UNitTests
 {
-    public class Tests
+    public class CollectionTests
     {
-        [SetUp]
-        public void Setup()
+
+        [Test]
+        public void Test_Collection_EmptyConstructor()
         {
+            var coll = new Collection<int>();
+            Assert.AreEqual(coll.ToString(), "[]");
         }
 
         [Test]
-        public void Test1()
+        public void Test_Collection_ConstructorSingleItem()
         {
-            Assert.Pass();
+            var coll = new Collection<int>(5);
+            Assert.AreEqual(coll.ToString(), "[5]");
         }
+
+        [Test]
+        public void Test_Collection_ConstructorMultipleItems()
+        {
+            var coll = new Collection<int>(5, 6);
+            Assert.AreEqual(coll.ToString(), "[5, 6]");
+        }
+
+        [Test]
+        public void Test_Collection_CountAndCapacity()
+        {
+            var coll = new Collection<int>(5, 6);
+            Assert.AreEqual(coll.Count, 2, "Check for count");
+            Assert.That(coll.Capacity, Is.GreaterThanOrEqualTo(coll.Count));
+        }
+
     }
 }
